2aaab7e1aa37fe049b30fdaa66f0ee6d607a7283
batmanleo <92765344+batmanleo@users.noreply.github.com>
2026-02-05 20:06:43 -0800

    Update stringpulleraparter.java

--- stringpulleraparter.java
+++ stringpulleraparter.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 //this is witten by Leo Adira
@@ -7,14 +8,16 @@ public static void main(String[] args) {
 //this splits the premade sentence 
     String test = "the quick brown fox jumped over the lazy dog";
     System.out.println("the quick brown fox jumped over the lazy dog");
-String[] burger = test.split(". [a-z]");
+String[] burger = test.split(" [a-z]");
+System.out.println(Arrays.toString(burger));
 System.out.println(burger.length);
 
 //this trys it's best to split the terrible user made sentence 
     Scanner keybord = new Scanner(System.in);
-System.out.println("please enter a sentence");
-String test2 = keybord.nextLine(); 
-burger = test2.split(". [a-z]");
+    System.out.println("please enter a sentence");
+    String test2 = keybord.nextLine(); 
+burger = test2.split(" [a-z]");
+System.out.println(Arrays.toString(burger));
 System.out.println(burger.length);
 
 }
