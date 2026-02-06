8651ce2158710f2e8689e515c58b94c377dc13f7
batmanleo <92765344+batmanleo@users.noreply.github.com>
2026-02-05 17:46:03 -0800

    Add files via upload

---
+++ stringpulleraparter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//this is witten by Leo Adira
+public class stringpulleraparter {
+public static void main(String[] args) {
+ 
+//this splits the premade sentence 
+    String test = "the quick brown fox jumped over the lazy dog";
+    System.out.println("the quick brown fox jumped over the lazy dog");
+String[] burger = test.split(". [a-z]");
+System.out.println(burger.length);
+
+//this trys it's best to split the terrible user made sentence 
+    Scanner keybord = new Scanner(System.in);
+System.out.println("please enter a sentence");
+String test2 = keybord.nextLine(); 
+burger = test2.split(". [a-z]");
+System.out.println(burger.length);
+
+}
+}
