e5ea141483f463ca6255d11a99c9de0408d359f3
batmanleo <92765344+batmanleo@users.noreply.github.com>
2026-04-03 18:15:43 -0700

    Add files via upload

---
+++ debugproject/HelloWorldBad.java
@@ -0,0 +1,12 @@
+
+class HelloWorldBad {
+    public static void main(String[] args) {
+        
+        for (int i=0;i<25;i++){
+            System.out.println("the value of i is "+i);
+            if (i=1){
+                System.out.println("i is equal to 1");
+            }
+        }    
+        }
+    }
---
+++ debugproject/error.java
@@ -0,0 +1,25 @@
+/*
+Error filled file created by
+Kimg@techtrepacademy.com
+for my java class to learn troubleshooting on
+*/
+public class Main {
+    System.out.println("hello everybody");
+    System.out.println("am I doing this correctly");
+    for (int i=0;i<10;i++){
+        System.out.printlm("the number i is "+i);
+    }
+
+    For (Int i=0:i>10;i++){
+        System.out.println("the new number i is "+i);
+    }
+    if (i==0){
+        System.out.print("i is equal to zero");
+
+    }else {
+        system.out.print("i is not equal to zero");
+
+    }
+    //what other errors can I put into a source code file?
+
+}
