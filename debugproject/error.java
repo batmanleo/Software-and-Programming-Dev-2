aa7dfac3e5ea2cc95b3f27c84ffe12fe2612210c
batmanleo <92765344+batmanleo@users.noreply.github.com>
2026-04-03 18:24:01 -0700

    Add files via upload

--- debugproject/error.java
+++ debugproject/error.java
@@ -3,23 +3,28 @@ Error filled file created by
 Kimg@techtrepacademy.com
 for my java class to learn troubleshooting on
 */
-public class Main {
+
+public class error {
+public static void main(String[] args) {
     System.out.println("hello everybody");
     System.out.println("am I doing this correctly");
     for (int i=0;i<10;i++){
-        System.out.printlm("the number i is "+i);
+        System.out.println("the number i is "+i);
     }
 
-    For (Int i=0:i>10;i++){
+    for (int i=0;i<10;i++){
         System.out.println("the new number i is "+i);
-    }
-    if (i==0){
-        System.out.print("i is equal to zero");
+   
+if (i==0){
+        System.out.println("i is equal to zero ");
 
     }else {
-        system.out.print("i is not equal to zero");
+        System.out.println("i is not equal to zero ");
 
     }
+    }
+    
     //what other errors can I put into a source code file?
 
+}
 }
